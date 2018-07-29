f723cdec28d34251bfbe2d69c1fc5b420659d719
lauratan <laura.tan108@gmail.com>
2018-07-29 21:20:10 +0000

    Added calendar on sitters profile page

---
+++ app/javascript/components/Availability.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import DayPicker, { DateUtils } from 'react-day-picker';
+
+export default class Availability extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        selectedDays: [],
+        year: [],
+        month: [],
+        day: [],
+        finalResult: []
+      }
+
+      //Push each dates from db into an array (change selectedDays[] -> not a state?)
+      this.props.dates.forEach( (date) => {
+        this.state.selectedDays.push(date);
+      })
+
+      //Substring each date into y/m/d (refactor?)
+      this.state.selectedDays.forEach( (date) => {
+        this.state.year.push(date.substring(0, 4))
+        this.state.month.push(date.substring(6, 7))
+        this.state.day.push(date.substring(8, 10))
+      })
+
+      //Show disabled days if not selected for one month prior
+      this.state.finalResult.push({
+        after: new Date(2018, 5, 30),
+        before: new Date()  
+      })
+
+      //Show disabled days from today until the first day it is available
+      this.state.finalResult.push({
+        after: new Date(),
+        before: new Date(Number(this.state.year[0]), Number(this.state.month[0]) - 1, Number(this.state.day[0]))  
+      })
+      
+      //Loop through the 3 arrays and return the correct date format 
+      for (let i = 1; i < this.state.year.length; i++) {
+        let after = new Date(Number(this.state.year[i-1]), Number(this.state.month[i-1]) - 1, Number(this.state.day[i-1]))
+        let before = new Date(Number(this.state.year[i]), Number(this.state.month[i]) - 1, Number(this.state.day[i]))
+        let result = {
+          after: after,
+          before: before
+        }
+        this.state.finalResult.push(result)
+      }
+
+      //Show disabled days from the last avail date until the next month
+      this.state.finalResult.push({
+        after: new Date(Number(this.state.year[this.state.year.length-1]), Number(this.state.month[this.state.year.length-1]) - 1, Number(this.state.day[this.state.year.length-1])),  
+        before: new Date(2018, Number(this.state.month[0]), 1)  
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <DayPicker
+          disabledDays = {this.state.finalResult}
+          />
+        </div>
+      );
+    }
+  }
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,72 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users = User.create([
-    {first_name: 'Alice',
-    last_name: 'Jones',
-    email: 'alice@alice.com',
-    password_digest: '1234',
-    avatar: ''
+    {
+        first_name: 'Alice',
+        last_name: 'Jones',
+        email: 'alice@alice.com',
+        password_digest: '1234',
+        avatar: ''
     },
-    {first_name: 'Bob',
-    last_name: 'Bay',
-    email: 'bob@bob.com',
-    password_digest: '1234',
-    avatar: ''
+    {
+        first_name: 'Bob',
+        last_name: 'Bay',
+        email: 'bob@bob.com',
+        password_digest: '1234',
+        avatar: ''
+    },
+    {
+        first_name: 'Charlie',
+        last_name: 'Compute',
+        email: 'charlie@charlie.com',
+        password_digest: '1234',
+        avatar: ''
+   }
+])
+
+sitters = Sitter.create([
+    {
+        name: 'Alice',
+        address: '198 W Hastings St. Vancouver, BC V6B 1H2',
+        summary: "I've been a dog mom for 5 years, I love doggies.",
+        rules: 'Prefer small and medium dogs',
+        price: 30,
+        latitude: 49.282223,
+        longitude: -123.109442,
+        user_id: 1
+    },
+    {
+        name: 'Charlie',
+        address: 'Eihu Lane, Vancouver, BC V6G, Canada',
+        summary: "I've been a dog mom for 3 years, I love doggies.",
+        rules: 'Prefer small dog',
+        price: 30,
+        latitude: 49.290625,
+        longitude: -123.133273,
+        user_id: 3
     }
 ])
 
-sitters = Sitter.create({
-    name: 'Alice',
-    address: '198 W Hastings St. Vancouver, BC V6B 1H2',
-    summary: "I've been a dog mom for 5 years, I love doggies.",
-    rules: 'Prefer small and medium dogs',
-    price: 30,
-    latitude: 49.282223,
-    longitude: -123.109442,
-    user_id: 1
-})
-
 availabilities = Availability.create([
     {
-    sitter_id: 1,
-    avail_date: '2018-08-09T19:00:00.000Z'
+        sitter_id: 1,
+        avail_date: '2018-08-09T19:00:00.000Z'
     },
     {
-    sitter_id: 1,
-    avail_date: '2018-08-10T19:00:00.000Z'
+        sitter_id: 1,
+        avail_date: '2018-08-10T19:00:00.000Z'
     },
     {
-    sitter_id: 1,
-    avail_date: '2018-08-19T19:00:00.000Z'
+        sitter_id: 1,
+        avail_date: '2018-08-19T19:00:00.000Z'
     },
     {
-    sitter_id: 1,
-    avail_date: '2018-08-22T19:00:00.000Z'
+        sitter_id: 1,
+        avail_date: '2018-08-22T19:00:00.000Z'
     },
     {
-    sitter_id: 1,
-    avail_date: '2018-08-28T19:00:00.000Z'
+        sitter_id: 1,
+        avail_date: '2018-08-28T19:00:00.000Z'
     }
 ])
 
