b26190f32090bf8d1c2342e3402a9c69ca2dbca7
Carmen Shing <carmenshing@gmail.com>
2018-07-30 03:29:54 +0000

    implement current_user & current_sitter control flow for login/logout/become a sitter/my sitter page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,14 @@ class ApplicationController < ActionController::Base
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
-	helper_method :current_user
+
+	def current_sitter
+		if current_user
+			@current_sitter = Sitter.find_by(user_id: current_user.id)
+		end
+	end
+
+	helper_method :current_user, :current_sitter
 	
 	def authorize
 		redirect_to '/login/new' unless current_user
