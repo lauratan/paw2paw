5334028660618fcb3e0a89045e385e6ffe95730b
cshing <carmenshing@gmail.com>
2018-07-31 23:25:09 -0700

    fix bug in params passed in session#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by_email(params[:email].downcase.strip)
-    if user && user.authenticate_with_credentials(params[:email].downcase.strip, params[:password])
+    user = User.find_by_email(params[:session][:email].downcase.strip)
+    if user && user.authenticate_with_credentials(params[:session][:email].downcase.strip, params[:session][:password])
       session[:user_id] = user.id
       redirect_to sitters_path
     else
