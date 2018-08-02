485e4e2d8823f270e624b1c5a6796118ccd286f3
cshing <carmenshing@gmail.com>
2018-08-01 19:12:12 -0700

    add private params to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,14 +10,23 @@ class BookingsController < ApplicationController
     @sitter = Sitter.find params[:sitter_id]
     @user = User.find_by(email: params[:booking][:email])
 
-    @booking = Booking.new({
-      user_id: @user.id,
-      sitter_id: @sitter.id
-    })
+    @booking = Booking.new(booking_params)
 
     if @booking.save! 
       redirect_to new_sitter_booking_booking_date_path(@sitter.id, @booking.id), notice: 'The booking request has been sent to the sitter'
     end
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def booking_params
+      params.require(:booking).permit(
+        :dog_breed, 
+        :dog_size, 
+        :dog_age, 
+        :dog_gender, 
+        :info
+      ).merge(user_id:@user.id, sitter_id:@sitter.id)
+    end
 
 end
