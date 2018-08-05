48986b35a2d72d2ff6d878608c092871e845f545
Mindyli1120 <mindyli@Yus-MacBook-Air.hitronhub.home>
2018-08-05 16:36:13 -0700

    fixed the bug of redirect to the sitters#show without showing the photo upload page

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -33,7 +33,7 @@ class SittersController < ApplicationController
     #to add img field for house pics
     
     if @sitter.save
-      redirect_to sitter_path(@sitter.id), notice: 'New sitter created!'
+      redirect_to new_sitter_photo_path(@sitter.id), notice: 'New sitter created!'
     else
       render :new
     end
